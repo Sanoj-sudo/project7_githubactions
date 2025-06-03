28344a00d05f8aa0a906a892209769984a1c533c
sanojsudo <sanoj.kumar@thoughtworks.com>
2025-06-03 14:18:23 +0530

    added files

--- backend.tf
+++ backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "somya-employee-008  "
-    key            = "eks-cluster/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table" 
-    encrypt        = true
+    bucket                  = "somya-employee-008"
+    key                     = "eks-cluster/terraform.tfstate"
+    dynamodb_table          = "terraform-lock-table"
   }
 }
